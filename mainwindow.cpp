d8a1e6a1afa171256102053012d7276d008bd0a6
IvanKorolev <Ivankorol1512@yandex.ru>
2026-02-15 16:26:28 +0300

    add tires, discs, casco, osago

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,9 +11,9 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->discsChb, &QCheckBox::stateChanged, this, &MainWindow::on_discsChb_checked);
     connect(ui->cascoChb, &QCheckBox::stateChanged, this, &MainWindow::on_cascoChb_checked);
     connect(ui->osagoChb, &QCheckBox::stateChanged, this, &MainWindow::on_osagoChb_checked);
-    connect(ui->condChb, &QCheckBox::stateChanged, this, &MainWindow::on_condChb_checked);
-    connect(ui->signChb, &QCheckBox::stateChanged, this, &MainWindow::on_signChb_checked);
-    connect(ui->lightsChb, &QCheckBox::stateChanged, this, &MainWindow::on_lightsChb_checked);
+    // connect(ui->condChb, &QCheckBox::stateChanged, this, &MainWindow::on_condChb_checked);
+    // connect(ui->signChb, &QCheckBox::stateChanged, this, &MainWindow::on_signChb_checked);
+    // connect(ui->lightsChb, &QCheckBox::stateChanged, this, &MainWindow::on_lightsChb_checked);
 
 }
 
@@ -38,6 +38,28 @@ void MainWindow::on_tiresChb_checked(int state){
 void MainWindow::on_discsChb_checked(int state){
     if (state == Qt::Checked){
         QPixmap image("/home/ivankorolev/Изображения/discs.jpeg");
+        ui->imageLbl->setPixmap(image);
+    }
+    else{
+        ui->imageLbl->clear();
+    }
+}
+
+
+void MainWindow::on_cascoChb_checked(int state){
+    if (state == Qt::Checked){
+        QPixmap image("/home/ivankorolev/Изображения/casco.jpeg");
+        ui->imageLbl->setPixmap(image);
+    }
+    else{
+        ui->imageLbl->clear();
+    }
+}
+
+
+void MainWindow::on_osagoChb_checked(int state){
+    if (state == Qt::Checked){
+        QPixmap image("/home/ivankorolev/Изображения/osago.png");
         ui->imageLbl->setPixmap(image);
     }
     else{
