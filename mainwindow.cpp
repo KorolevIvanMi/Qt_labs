d6cfce7dbdf23a6afbd517322cb4a8e8af73025b
IvanKorolev <Ivankorol1512@yandex.ru>
2026-02-19 17:21:12 +0300

    g

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     SetupData();
+    connect(ui->resultBtn, &QPushButton::clicked, this, &MainWindow::result_btn_clicked);
 
 }
 
@@ -16,30 +17,82 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::SetupData(){
-    QList<QRadioButton*> season_childrens = ui->seasonGb->findChildren<QRadioButton*>();
-    QList<QRadioButton*> rods_childrens = ui->rod_typeGb->findChildren<QRadioButton*>();
-    QList<QRadioButton*> line_childrens = ui->fishing_line_typeGb->findChildren<QRadioButton*>();
-    QList<QRadioButton*> diametrs_childrens = ui->fishing_line_diametrGb->findChildren<QRadioButton*>();
+    season_childrens = ui->seasonGb->findChildren<QRadioButton*>();
+    rods_childrens = ui->rod_typeGb->findChildren<QRadioButton*>();
+    line_childrens = ui->fishing_line_typeGb->findChildren<QRadioButton*>();
+    diametrs_childrens = ui->fishing_line_diametrGb->findChildren<QRadioButton*>();
 
     int season_name_index = 0;
     foreach (QRadioButton* season_radio, season_childrens) {
         season_radio->setText(seasons[season_name_index++]);
-        connect(season_radio, &QRadioButton::clicked, this, &MainWindow::on_radio_clicked());
+        connect(season_radio, &QRadioButton::clicked, this, &MainWindow::radio_clicked);
     }
 
     int rods_type_index = 0;
-    foreach (QRadioButton* rode_type_radio, rods_childrens) {
-        rode_type_radio->setText(rods_types[rods_type_index++]);
+    foreach (QRadioButton* rod_type_radio, rods_childrens) {
+        rod_type_radio->setText(rods_types[rods_type_index++]);
+        connect(rod_type_radio, &QRadioButton::clicked, this, &MainWindow::radio_clicked);
     }
 
     int line_types_index = 0;
     foreach (QRadioButton* line_type_radio, line_childrens) {
         line_type_radio->setText(line_types[line_types_index++]);
+        connect(line_type_radio, &QRadioButton::clicked, this, &MainWindow::radio_clicked);
     }
 
     int diametr_index = 0;
     foreach (QRadioButton* diametr_radio,diametrs_childrens) {
-        QString diametr_str; diametr_str.setNum(line_diametr[diametr_index++]);
-        diametr_radio->setText(diametr_str);
+
+        diametr_radio->setText( line_diametr[diametr_index++]);
+        connect(diametr_radio, &QRadioButton::clicked, this, &MainWindow::radio_clicked);
     }
+}
+
+
+void MainWindow::radio_clicked(){
+    ui->result_sumLbl->clear();
+}
+
+
+void MainWindow::result_btn_clicked(){
+    QRadioButton* active_season = find_active_radio_btn(season_childrens);
+    int season_price = find_price_by_name(active_season->text(), seasons, seasons_price, 2);
+    QString season_price_str; season_price_str.setNum(season_price);
+
+    QRadioButton* active_rod_type = find_active_radio_btn(rods_childrens);
+    int rod_type = find_price_by_name(active_rod_type->text(), rods_types, rods_types_price, 3);
+    QString rod_type_str; rod_type_str.setNum(rod_type);
+
+    QRadioButton* active_line = find_active_radio_btn(line_childrens);
+    int line = find_price_by_name(active_line->text(), line_types,line_types_price,3 );
+    QString line_str; line_str.setNum(line);
+
+    QRadioButton* active_diametr = find_active_radio_btn(diametrs_childrens);
+    int diametr = find_price_by_name(active_diametr->text(), line_diametr, line_diametr_price, 4);
+    QString diametr_str; diametr_str.setNum(diametr);
+
+
+
+
+}
+
+
+QRadioButton* MainWindow::find_active_radio_btn(QList<QRadioButton*> list) {
+    foreach (QRadioButton* radio, list) {
+        if (radio->isChecked()){
+            return radio;
+        }
+    }
+    return list[0];
+}
+
+
+int MainWindow::find_price_by_name(QString name ,QString array[], int prices[], int size){
+    int res_price = 0;
+    for(int i = 0 ; i < size; i ++){
+        if (name == array[i]){
+            res_price = prices[i];
+        }
+    }
+    return res_price;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "qradiobutton.h"
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -18,31 +19,48 @@ public:
     ~MainWindow();
 
     void SetupData();
+    QRadioButton* find_active_radio_btn(QList<QRadioButton*>);
+    int find_price_by_name(QString, QString[], int[],int );
 
 private:
     Ui::MainWindow *ui;
+
+    QList<QRadioButton*> season_childrens;
+    QList<QRadioButton*> rods_childrens;
+    QList<QRadioButton*> line_childrens;
+    QList<QRadioButton*> diametrs_childrens;
+
     QString seasons[2] = {
         "Зима",
         "Лето"
     };
+    int seasons_price[2]{1000,600};
+
     QString rods_types[3] = {
         "Инерционная",
         "Безынерционная",
         "Мультипликаторная"
     };
+    int rods_types_price[3] = {500, 1000, 1300};
+
     QString line_types[3] = {
         "Монофильная",
         "Плетёная",
         "Флюрокарбон"
     };
-    float line_diametr[4] = {
-        0.1,
-        0.15,
-        0.2,
-        0.25
+    int line_types_price[3] = {50, 100, 150};
+
+    QString line_diametr[4] = {
+        "0.1",
+        "0.15",
+        "0.2",
+        "0.25"
     };
+    int line_diametr_price[4] = {20, 30, 40, 50};
+    int meters = 0;
 
 private slots:
-    void on_groubBox_clicked();
+    void radio_clicked();
+    void result_btn_clicked();
 };
 #endif // MAINWINDOW_H
